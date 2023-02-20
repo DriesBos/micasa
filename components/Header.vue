f5a98ec6236a79799718faec5bfdacec999f1988
DriesBos <info@driesbos.com>
2023-02-20 14:48:57 +0000

    added dynamic header

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,48 @@
+<script setup>
+const storyblokApi = useStoryblokApi();
+const { data } = await storyblokApi.get('cdn/stories/config', {
+  version: 'draft',
+  resolve_links: 'url',
+});
+
+const headerMenu = ref(null);
+headerMenu.value = data.story.content.header_menu;
+</script>
+
 <template>
-  <header class="w-full h-24 bg-[#f7f6fd]">
-    <div class="container h-full mx-auto flex items-center justify-between">
+  <header class="header">
+    <div class="header-Logo">
       <NuxtLink to="/">
-        <h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok Nuxt</h1>
+        <h1>Nuxt Storyblok Boilerplate</h1>
       </NuxtLink>
-      <nav>
-        <ul class="flex space-x-8 text-lg font-bold">
-          <li>
-            <NuxtLink to="/blog" class="hover:text-[#50b0ae]">Blog</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/about" class="hover:text-[#50b0ae]">About</NuxtLink>
+    </div>
+    <div class="header-Nav">
+      <nav v-if="headerMenu">
+        <ul>
+          <li v-for="blok in headerMenu" :key="blok._uid">
+            <NuxtLink :to="blok.link.cached_url">
+              {{ blok.link.story.name }}
+            </NuxtLink>
           </li>
         </ul>
       </nav>
     </div>
   </header>
 </template>
+
+<style lang="sass" scoped>
+.header
+  display: flex
+  justify-content: space-between
+  &-Logo
+    padding: 1em
+  &-Nav
+    ul
+      display: flex
+      li
+        padding: 1em
+  a
+    text-decoration: none
+    &:hover
+      text-decoration: underline
+</style>
