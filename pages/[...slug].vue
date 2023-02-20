1d752cc505904bc14a6ad9afdf5e07bf788f41cc
DriesBos <info@driesbos.com>
2023-02-20 14:19:47 +0000

    Added stories + header

---
+++ pages/[...slug].vue
@@ -0,0 +1,21 @@
+<script setup>
+const { slug } = useRoute().params;
+
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join('/') : 'home',
+  { version: 'draft' }
+);
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
+
+<!-- For index.vue if needed -->
+<!-- <script setup>
+const story = await useAsyncStoryblok('home', { version: 'draft' });
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template> -->
