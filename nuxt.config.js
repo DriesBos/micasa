8c9e0043b0e63f350bb756c67685095e07e12bfb
DriesBos <info@driesbos.com>
2023-02-20 14:49:09 +0000

    added styling structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@import "@/assets/styles/vars/variables.sass"',
+        },
+      },
+    },
+  },
+
+  css: [
+    '~/assets/styles/reset.css',
+    '~/assets/styles/form-reset.css',
+    '~/assets/styles/typography.sass',
+    '~/assets/styles/main.sass',
+  ],
+
   modules: [['@storyblok/nuxt', { accessToken: 'xqxGf5K27JJeyyfVi3w3Agtt' }]],
 });
