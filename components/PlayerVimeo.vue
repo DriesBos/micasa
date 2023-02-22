6541d01246b3de3e0556860ee92a56ae6a8b01bf
DriesBos <info@driesbos.com>
2023-02-22 12:29:38 +0000

    added videoplayer components (youtube+vimeo)

---
+++ components/PlayerVimeo.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- prettier-ignore -->
+  <iframe
+    :src="'https://player.vimeo.com/video/' + videoid + '?autoplay=' + autoplay + '&muted=true'"
+    frameborder="0"
+    allow="autoplay"
+    transparent="true"
+    autopause="true"
+    loop="true"
+    muted="true"
+    playsinline="true"
+    title="false"
+    webkitallowfullscreen
+    mozallowfullscreen
+    allowfullscreen
+  />
+</template>
+
+<script setup>
+const props = defineProps({ videoid: String, autoplay: String });
+</script>
