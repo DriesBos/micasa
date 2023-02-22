d64da661896f4edd7725acc109bbc02ff95ffb63
DriesBos <info@driesbos.com>
2023-02-22 17:46:13 +0000

    added hooks + animation examples

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,32 @@
 <template>
   <main>
     <TheHeader />
-    <div class="cursor" />
+    <div class="cursor" ref="cursor" />
     Mouse position is at: {{ x }}, {{ y }}
     <slot />
   </main>
 </template>
 
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue';
+import { onMounted, onUnmounted } from 'vue';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { useMouse } from './../composables/mouse';
+import { useMouse } from '../hooks/mouse';
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 const { x, y } = useMouse();
+
+function moveCursor(props) {
+  gsap.to('.cursor', 0.1, {
+    opacity: 1,
+    left: props.clientX,
+    top: props.clientY,
+    ease: 'ease',
+  });
+}
+
+onMounted(() => window.addEventListener('mousemove', moveCursor));
+onUnmounted(() => window.removeEventListener('mousemove', moveCursor));
 </script>
