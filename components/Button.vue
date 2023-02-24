8cc9aef00db1faeb2403c060df9b0eaf7fe4d87b
DriesBos <info@driesbos.com>
2023-02-24 12:56:01 +0000

    btn active prop

--- components/Button.vue
+++ components/Button.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="button">
+  <div class="button" :class="{ active: active }">
     <slot />
   </div>
 </template>
 
+<script setup>
+const props = defineProps({ active: Object });
+</script>
+
 <style lang="sass" scoped>
 .button
   display: flex
-  background: black
-  color: white
+  border: 2px solid black
   border-radius: 1000px
-  padding: .5em 2em
+  padding: .25em 1em
   cursor: pointer
   font-family: "Futura PT", Helvetica, Arial, sans-serif
   font-size: 1rem
+  font-weight: bold
+  letter-spacing: .025rem
+  &.active
+    background: black
+    color: white
+    border: 2px solid transparent
 </style>
