0105cf17add74c03f4c9fb15d4daab753eb36a91
DriesBos <info@driesbos.com>
2023-03-06 11:01:38 +0000

    style: footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,37 +1,26 @@
 <template>
   <footer class="footer cursorInvert">
-    <div class="footer-Left">
-      <div class="footer-Left_Logo displayFont cursorInteract">
+    <div>
+      <div class="footer-Logo displayFont cursorInteract">
         <NuxtLink to="/">
           <h1>MiCasa</h1>
         </NuxtLink>
       </div>
     </div>
 
-    <div class="footer-Right">
-      <div class="footer-Right_Top">
-        <div>
-          <a class="cursorInteract" href="https://www.instagram.com">Twitter</a>
-          <a class="cursorInteract" href="https://www.instagram.com"
-            >Instagram</a
-          >
-        </div>
-        <div>
-          <a class="cursorInteract" href="https://www.instagram.com"
-            >info@micasa.com</a
-          >
-          <a class="cursorInteract" href="https://www.instagram.com"
-            >+351242424240</a
-          >
-        </div>
-      </div>
+    <div>
+      <a class="cursorInteract" href="https://www.instagram.com">Twitter</a>
+      <a class="cursorInteract" href="https://www.instagram.com">Instagram</a>
+      <a class="cursorInteract" href="https://www.instagram.com"
+        >info@micasa.com</a
+      >
+    </div>
 
-      <div class="footer-Right_Bottom newsletter cursorInteract">
-        <div class="newsletter-Title"><p>Newsletter</p></div>
-        <div class="newsletter-Input">
-          <input placeholder="Your email.." />
-          <nuxt-icon class="icon icon-Arrow_right" name="arrow-right" />
-        </div>
+    <div class="footer-Right_Bottom newsletter cursorInteract">
+      <div class="newsletter-Title"><p>Newsletter</p></div>
+      <div class="newsletter-Input">
+        <input placeholder="Your email.." />
+        <nuxt-icon class="icon icon-Arrow_right" name="arrow-right" />
       </div>
     </div>
   </footer>
@@ -43,47 +32,27 @@
   flex-wrap: nowrap
   background: $bg-black
   color: $color-light
-  padding: var(--spacing-sections) var(--spacing-sides) var(--spacing-base) var(--spacing-sides)
-  column-gap: 1rem
+  padding: var(--spacing-sections) var(--spacing-sides) 2rem var(--spacing-sides)
   & > div
     display: flex
     flex-direction: column
     justify-content: flex-end
-    flex-basis: 50%
-    column-gap: 1rem
-  &-Left_Logo
+    flex-basis: 33.3333%
+    a, p
+      color: $color-light
+      font-size: 2rem
+  &-Logo
     h1
       font-size: 4rem
       line-height: 0.6
       color: $color-light
-  &-Right
-    a, p
-      color: white
-      font-size: 2rem
-    & > div
-      flex-grow: 1
-    &_Top
-      display: flex
-      flex-wrap: nowrap
-      margin-bottom: var(--spacing-base)
-      & > div
-        flex-basis: 50%
-        display: flex
-        flex-direction: column
-        column-gap: 1rem
-    &_Bottom
-      display: flex
-      flex-direction: column
-      & > div
-        &:first-child
-          margin-bottom: .5rem
 
 .newsletter
   &-Input
     display: flex
-    border: 2px solid white
+    border: 2px solid $color-light
     font-size: 2rem
-    height: 4rem
+    height: 3rem
     input
       background: transparent
       flex-grow: 1
@@ -92,13 +61,13 @@
       display: flex
       align-items: center
       justify-content: center
-      width: 4rem
-      height: 4rem
+      width: 3rem
+      height: 100%
       border-left: 2px solid white
-      color: white !important
+      color: $color-light
     svg
-      color: white !important
       width: 2rem !important
       height: 2rem !important
+      margin-bottom: 0 !important
       object-fit: contain
 </style>
