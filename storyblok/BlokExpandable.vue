8b37f80e976942866c6f08700157f3202920ffee
DriesBos <info@driesbos.com>
2023-03-08 12:11:52 +0000

    collapse animation

--- storyblok/BlokExpandable.vue
+++ storyblok/BlokExpandable.vue
@@ -6,25 +6,26 @@
       </div>
       <h2>{{ blok.title }}</h2>
     </div>
-    <Transition>
-      <div v-if="show" class="expandable-Bottom">
-        <markdown :content="blok.text" />
-      </div>
-    </Transition>
+    <Collapse :when="show" class="collapse expandable-Bottom">
+      <markdown :content="blok.text" />
+    </Collapse>
   </div>
 </template>
 
 <script setup>
+import { Collapse } from 'vue-collapsed';
+
 defineProps({ blok: Object });
 
 const show = ref(false);
 </script>
 
 <style lang="sass" scoped>
+
+
 .expandable
   display: flex
   flex-direction: column
-  // padding: 0 4rem
   &-Top
     display: flex
     flex-wrap: nowrap
@@ -38,18 +39,30 @@ const show = ref(false);
       font-size: 4rem
   &-Bottom
     overflow: hidden
+    opacity: 0
     .markdown
       max-width: 60em
   &.active
     .expandable-Top_Icon
       transform: rotate(135deg)
 
+.collapse
+  transition: height var(--vc-auto-duration) ease-out, opacity var(--vc-auto-duration) ease-out
 
-// Expand Animation
-.v-enter-active, .v-leave-active
-  transition: opacity 0.33s ease
+.collapse[data-collapse='expanded'],
+.collapse[data-collapse='expanding']
+  opacity: 1
 
-
-.v-enter-from, .v-leave-to
+// .collapse[data-collapse='collapsed'],
+.collapse[data-collapse='collapsing']
   opacity: 0
+
+
+// // Expand Animation
+// .v-enter-active, .v-leave-active
+//   transition: opacity 0.33s ease
+
+
+// .v-enter-from, .v-leave-to
+//   opacity: 0
 </style>
