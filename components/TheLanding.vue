2bc970921caded75ee0e9d5716115e0c88dabf2f
DriesBos <info@driesbos.com>
2023-03-09 18:46:38 +0000

    landing: added half state

--- components/TheLanding.vue
+++ components/TheLanding.vue
@@ -1,45 +1,65 @@
 <template>
   <Teleport to="body">
-    <div
-      class="landing"
-      :class="{ active: isActive }"
-      @click="isActive = false"
-    >
-      <div class="landing-Content">
+    <div class="landing" :class="currentState">
+      <div
+        class="landing-Content cursorInteract"
+        @click="currentState = 'hidden'"
+        @mouseenter="
+          currentState == 'hidden'
+            ? (currentState = 'hidden')
+            : (currentState = 'half')
+        "
+        @mouseleave="
+          currentState == 'hidden'
+            ? (currentState = 'hidden')
+            : (currentState = 'full')
+        "
+      >
         <h1 class="displayFont">MiCasa</h1>
         <p>{{ data.story.content.landing_text }}</p>
       </div>
       <div class="landing-Image">
-        <img :src="data.story.content.landing_image.filename" />
+        <div class="landing-Image_Wrapper">
+          <img :src="data.story.content.landing_image.filename" />
+        </div>
       </div>
     </div>
   </Teleport>
 </template>
+
+<script setup>
+const props = defineProps({ activeState: String });
+
+const storyblokApi = useStoryblokApi();
+const { data } = await storyblokApi.get('cdn/stories/config', {
+  version: 'draft',
+});
+
+const currentState = ref(null);
+currentState.value = props.activeState;
+</script>
 
 <style lang="sass" scoped>
 .landing
   position: fixed
   top: 0
   left: 0
-  width: calc(100vw - .5rem)
+  width: 100vw
   height: 100vh
-  background: green
   display: flex
   align-items: center
   justify-content: center
-  transform: translateX(-100%) scale(0.96)
-  transition: transform .33s ease-in-out, border-radius .33s ease-in-out
-  background: $bg-yellow
-  border-radius: 0 2rem 2rem 0
+  transition: transform .33s ease-in-out, background .33s ease-in-out
   overflow: hidden
   z-index: +1
-  &.active
-    transform: translateX(0) scale(1)
-    border-radius: 0
+  pointer-event: none
   &-Content
     text-align: center
     z-index: +1
     max-width: 55rem
+    pointer-event: auto
+    transition: transform .33s ease-in-out, background .33s ease-in-out, opacity .165s ease-in-out
+    opacity: 1
     h1
       color: $color-light
       font-size: 33vmin
@@ -57,25 +77,43 @@
       hyphens: none
   &-Image
     position: absolute
-    left: 2rem
-    top: 2rem
-    right: 2rem
-    bottom: 2rem
-    background: black
+    left: 0
+    top: 0
+    right: 0
+    bottom: 0
+    padding: 2rem
+    background: $bg-yellow
+    transition: transform .33s ease-in-out
+    &_Wrapper
+      background: $color
+      width: 100%
+      height: 100%
     img
       width: 100%
       height: 100%
       object-fit: cover
       opacity: .66
+
+  &.full
+    background: hsl(100, 100%, 0%, 0.6)
+    .landing
+      &-Image
+        transform: translateX(0) scale(1)
+      &-Content
+        transform: translateX(0) scale(1)
+  &.half
+    background: hsl(100, 100%, 0%, 0.2)
+
+    .landing
+      &-Image
+        transform: translateX(0 - 4rem) scale(1)
+      &-Content
+        transform: translateX(0) scale(1)
+        opacity: .8
+  &.hidden
+    background: hsl(100, 100%, 0%, 0)
+    transform: translateX(-100%) scale(0.96)
+    .landing
+      &-Content
+        opacity: 0
 </style>
-
-<script setup>
-const props = defineProps({ activeState: Boolean });
-
-const storyblokApi = useStoryblokApi();
-const { data } = await storyblokApi.get('cdn/stories/config', {
-  version: 'draft',
-});
-
-let isActive = ref(props.activeState);
-</script>
