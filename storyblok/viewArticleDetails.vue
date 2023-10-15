8505cc1b2b297abb68f4d923915896e9b9a5a834
DriesBos <info@driesbos.com>
2023-10-15 20:26:56 +0100

    Added details comp

---
+++ storyblok/viewArticleDetails.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="details">
+    <Markdown :content="blok.description" />
+    <Markdown :content="blok.details" />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ blok: Object });
+
+onMounted(() => {
+  console.log('mounted', props.blok);
+});
+</script>
+
+<style lang="sass">
+.details
+  margin: 0 var(--spacing-sides)
+  display: flex
+  gap: var(--spacing-three)
+  & > div
+    flex: 1
+    &.markdown p
+      text-align: left !important
+    &:last-child
+      max-width: 33%
+</style>
