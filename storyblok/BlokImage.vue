69f53dfd5711659c9758c13756747d866606187f
DriesBos <info@driesbos.com>
2023-10-16 20:59:57 +0100

    cleanup

--- storyblok/BlokImage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div v-editable="blok" class="imageBlok">
-    <img :src="blok.image.filename" :alt="blok.image.alt" />
-  </div>
-</template>
-
-<script setup>
-defineProps({ blok: Object });
-</script>
--- storyblok/BlokText.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div v-editable="blok" class="textBlok">
-    <Markdown :content="blok.content" />
-  </div>
-</template>
-
-<script setup>
-defineProps({ blok: Object });
-</script>
